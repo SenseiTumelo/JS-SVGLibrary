341225a72b28ba2e490f68eb2ce4296fc6ab6883
senseiTumelo <tmotjopi@gmail.com>
2022-01-19 14:16:35 +0200

    11

--- js/sight.js
+++ js/sight.js
@@ -3,6 +3,9 @@ class SVGElement{
     constructor(type){
         let type = this.type;
         let namespace = 'http://www.w3.org/2000/svg';
+        let node = document.createElementNS();
+        node.namespace.type;
+        return this;
     }
 
 
